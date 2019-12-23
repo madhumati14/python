a6484ee942127a598984868f337f4e5213c530a1
madhumati14 <57008680+madhumati14@users.noreply.github.com>
2019-12-24 03:20:35 +0530

    python

---
+++ Assignment1_5.py
@@ -0,0 +1,14 @@
+#Write a program which display 10 to 1 on screen.
+#Output : 10 9 8 7 6 5 4 3 2 1
+
+def display():
+	i=10;
+	while i>0:
+		print(i,end=" ");
+		i-=1;
+def main():
+	
+	display();
+
+if __name__=="__main__":
+	main()
