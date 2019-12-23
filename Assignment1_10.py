b369a7b89f6132db84c0e4e920b18856d0d55a4e
madhumati14 <57008680+madhumati14@users.noreply.github.com>
2019-12-24 03:29:39 +0530

    python

---
+++ Assignment1_10.py
@@ -0,0 +1,14 @@
+#Write a program which accept name from user and display length of its name.
+#Input : Marvellous
+#Output : 10
+
+def countlen(arr):
+
+	print (len(arr));
+
+def main():
+	arr=input(("enter the string"));
+	countlen(arr);
+
+if __name__=="__main__":
+	main()
