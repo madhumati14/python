0b217e3d8e3248a7d5a683120058b82acc7f2dde
madhumati14 <57008680+madhumati14@users.noreply.github.com>
2019-12-24 03:00:28 +0530

    Assignment1_1.py

---
+++ Assignment1_1.py
@@ -0,0 +1,10 @@
+#Write a program which contains one function named as Fun(). That function should display “Hello from Fun” on console.
+
+def fun():
+	print("hello");
+
+def main():
+	fun();
+
+if __name__=="__main__":
+	main();
